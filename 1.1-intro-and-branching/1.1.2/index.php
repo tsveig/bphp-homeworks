50c314f4f8425997815bcaf7ffcb6379347cc037
Anna <panova2006_89@mail.ru>
2019-06-04 09:33:03 +0300

    Update index.php

--- 1.1-intro-and-branching/1.1.2/index.php
+++ 1.1-intro-and-branching/1.1.2/index.php
@@ -1,17 +1,27 @@
-<?php
-$variable = 1.1;
-if (is_bool($variable) === TRUE) {
-    $type = 'bool';
-} elseif (is_float($variable) === TRUE) {
-    $type = 'float';
-} elseif (is_int($variable) === TRUE) {
-    $type = 'int';
-} elseif (is_string($variable) === TRUE) {
-    $type = 'string';
-} elseif (is_null($variable) === TRUE) {
-    $type = 'null';
+<?php 
+$hour = date("H");
+$day = date("N");
+if($hour >= 6 && $hour < 11) {
+    $timeOfDay = 'Доброе утро!';
+    $img='0.jpg';
+} elseif ($hour >= 11 && $hour < 18) {
+    $timeOfDay = 'Добрый день!';
+    $img='1.jpg';
+} elseif ($hour >= 18 && $hour < 23) {
+    $timeOfDay = 'Добрый вечер!';
+    $img='2.jpg';
 } else {
-    $type = 'other';
+    $timeOfDay = 'Доброй ночи!';
+    $img='3.jpg';
+}
+switch ($day) {
+    case 1: $dayOfWeek = 'понедельник'; break;
+    case 2: $dayOfWeek = 'вторник'; break;
+    case 3: $dayOfWeek = 'среда'; break;
+    case 4: $dayOfWeek = 'четверг'; break;
+    case 5: $dayOfWeek = 'пятница'; break;
+    case 6: $dayOfWeek = 'суббота'; break;
+    case 7: $dayOfWeek = 'воскресение'; break;
 }
 ?>
 
@@ -19,9 +29,14 @@ if (is_bool($variable) === TRUE) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>bPHP - 1.1.1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>bPHP - 1.1.2</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <p><?="$variable is $type"?></p>
+<body style ="background-image:url(<?=$img?>)">
+    <div class="greeting">
+        <h1><?="$timeOfDay Сегодня - $dayOfWeek" ?></h1>
+    </div>
 </body>
 </html>
