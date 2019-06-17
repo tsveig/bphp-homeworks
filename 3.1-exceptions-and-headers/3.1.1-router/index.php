70bc1eac837c573e46a6640a174ddf0a677af0de
Anna <panova2006_89@mail.ru>
2019-06-17 18:57:38 +0300

    add homeworks

--- 3.1-exceptions-and-headers/3.1.1-router/index.php
+++ 3.1-exceptions-and-headers/3.1.1-router/index.php
@@ -5,3 +5,42 @@
  * @var array $availableLinks
  */
 $availableLinks = include './availableLinks.php';
+class WrongHeader extends Exception
+{
+}
+class PageNotFound extends Exception
+{
+}
+class Router
+{
+    public $page;
+    public function __construct($name)
+    {
+        $this->page = $name;
+    }
+    public function checkPageName()
+    {
+        if (!array_key_exists('page', $_GET)) {
+            throw new WrongHeader('Get request error');
+        }
+        if (!array_key_exists($_GET['page'], [
+            'article',
+            'news',
+            'about',
+            'main'
+        ])) {
+            throw new PageNotFound('Page is not exist');
+        }
+        return true;
+    }
+}
+$page = new Router('main');
+try {
+    $page->checkPageName();
+    echo "Вы находитесь на странице {$_GET['page']}";
+} catch (WrongHeader $e) {
+    header('Location: error.php', 400);
+} catch (PageNotFound $e) {
+    header('Location: 404.php', 404);
+}
+?>
